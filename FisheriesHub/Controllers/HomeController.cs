377e822b81edb689a1254b773064cbfbb2ef8367
Anvesh <anveshk31@gmail.com>
2018-06-07 23:00:35 -0500

    First Commit

---
+++ FisheriesHub/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FisheriesHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Tab1()
+        {
+            ViewBag.Message = "Waiting for content";
+
+            return View();
+        }
+
+        public ActionResult Tab2()
+        {
+            ViewBag.Message = "Waiting for content again";
+
+            return View();
+        }
+    }
+}
---
+++ FisheriesHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FisheriesHub.MvcApplication" Language="C#" %>
